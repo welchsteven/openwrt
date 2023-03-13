222d850d41f87aff623af6da27d049753c78b9ba
Steven Welch <welchsteven@gmail.com>
2023-03-13 16:26:11 -0600

    Initial attempt at adding mikrotik Groove support

--- target/linux/ath79/image/mikrotik.mk
+++ target/linux/ath79/image/mikrotik.mk
@@ -137,3 +137,14 @@ define Device/mikrotik_routerboard-wap-2nd
   IMAGE_SIZE := 16256k
 endef
 TARGET_DEVICES += mikrotik_routerboard-wap-2nd
+
+define Device/mikrotik_routerboard-groovea-52HPacn
+  $(Device/mikrotik_nor)
+  SOC := qca9556
+  DEVICE_MODEL := GrooveA 52 (wAP AC)
+  IMAGE_SIZE := 16256k
+  DEVICE_PACKAGES += kmod-ath10k-ct-smallbuffers ath10
+  SUPPORTED_DEVICES += rb-groovea-52hpacn
+endef
+TARGET_DEVICES += mikrotik_routerboard-groovea-52HPacn
+
